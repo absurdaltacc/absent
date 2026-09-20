165f2f4eb2627f61bef4f6bade885d1a30fded67
absurdaltacc <viroda2cnd@gmail.com>
2026-09-20 16:04:57 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,10 @@
+importScripts("https://cdn.jsdelivr.net/gh/Destroyed12121/Staticsj@main/JS/scramjet.all.js");
+const { ScramjetServiceWorker } = $scramjetLoadWorker();
+const scramjet = new ScramjetServiceWorker();
+self.addEventListener("fetch", (event) => {
+  event.respondWith((async () => {
+    await scramjet.loadConfig();
+    if (scramjet.route(event)) return scramjet.fetch(event);
+    return fetch(event.request);
+  })());
+});
